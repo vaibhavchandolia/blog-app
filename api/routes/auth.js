845e4a9c2a4ad66f9ac7ef1b24d54d60a8924d30
vaibhavchandolia <vaibhavchandolia52@gmail.com>
2023-06-17 18:48:57 +0530

    finished user actions

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -3,6 +3,8 @@ const User = require("../modules/User")
 const bcrypt = require('bcrypt')
 
 
+
+//REGISTER
 router.post("/register", async (req, res) => {
     try {
       const salt = await bcrypt.genSalt(10);
@@ -19,5 +21,22 @@ router.post("/register", async (req, res) => {
       res.status(500).json(err);
     }
   });
+
+  
+  //LOGIN
+  router.post("/login", async (req, res) => {
+    try{
+      const user = await User.findOne({username: req.body.username})
+      !user && res.status(400).json("Wrong credentials")
+      
+      const validated = await bcrypt.compare(req.body.password, user.password)
+      !validated && res.status(400).json("Wrong credentials")
+
+      const{password, ...others} = user._doc
+      res.status(200).json(others)
+    } catch(err){
+      res.status(500).json(err);
+    }
+  })
 
   module.exports = router;
